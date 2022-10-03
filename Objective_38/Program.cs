0d6bebffac18d8b73b62ac3b015203015bb41dd0
BSerge <serge.boskin@icloud.com>
2022-10-03 15:27:30 +0300

    add Objective_38

---
+++ Objective_38/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.Write("Введите количество элементов массива: ");
+int size = int.Parse(Console.ReadLine()!);
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.Write("Исходный массив: ");
+PrintArray(numbers);
+
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 1; i < numbers.Length; i++) 
+    {
+        if (numbers[i] > max) max = numbers[i];
+        if (numbers[i] < min) min = numbers[i];    
+    }
+Console.WriteLine($"Разница между максимальным и минимальным значением {max-min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++) 
+    {
+        numbers[i] = new Random().NextDouble()*(100-0)+0; //решение отсюда https://stackoverflow.com/questions/1064901/random-number-between-2-double-numbers
+    }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]} ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
